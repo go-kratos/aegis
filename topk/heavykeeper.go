67e0fbac21a060e3b6f82e6f1eb0926bd04ab553
Weizhen Wang <wangweizhen@pingcap.com>
2023-02-01 11:12:17 +0800

    topk: fix unsafe pointer conversion in the murmur3 (#32)
    * topk: fix unsafe pointer conversion in the murmur3
    
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>
    
    * update
    
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>
    
    ---------
    
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- topk/heavykeeper.go
+++ topk/heavykeeper.go
@@ -9,7 +9,7 @@ import (
 
 	"github.com/go-kratos/aegis/pkg/minheap"
 
-	"github.com/spaolacci/murmur3"
+	"github.com/twmb/murmur3"
 )
 
 const LOOKUP_TABLE = 256
@@ -74,7 +74,7 @@ func (topk *HeavyKeeper) Add(key string, incr uint32) bool {
 	// compute d hashes
 	for i, row := range topk.buckets {
 
-		bucketNumber := murmur3.Sum32WithSeed(keyBytes, uint32(i)) % uint32(topk.width)
+		bucketNumber := murmur3.SeedSum32(uint32(i), keyBytes) % uint32(topk.width)
 		fingerprint := row[bucketNumber].fingerprint
 		count := row[bucketNumber].count
 
