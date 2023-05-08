baff2f2a329c0ad03facfe7998d071a1ecc74bf9
songzhibin97 <718428482@qq.com>
2023-05-08 11:59:26 +0800

    Update bbr.go

--- ratelimit/bbr/bbr.go
+++ ratelimit/bbr/bbr.go
@@ -299,11 +299,10 @@ func (l *BBR) Allow() (ratelimit.DoneFunc, error) {
 	ms := float64(time.Millisecond)
 	return func(ratelimit.DoneInfo) {
 		//nolint
-		end := time.Now().UnixNano()
-		if start >= end {
+		rt := int64(math.Ceil(float64(time.Now().UnixNano()-start)) / ms)
+		if rt <= 0 {
 			return
 		}
-		rt := int64(math.Ceil(float64(time.Now().UnixNano()-start)) / ms)
 		l.rtStat.Add(rt)
 		atomic.AddInt64(&l.inFlight, -1)
 		l.passStat.Add(1)
